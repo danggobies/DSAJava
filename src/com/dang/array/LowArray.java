06a2bb155a37c9a6cb0ebddadbe93de6fd63cebd
dangnh <haidang.nguyen@olbius.vn>
2020-03-12 17:16:09 +0700

    init repo and bubble sort

---
+++ src/com/dang/array/LowArray.java
@@ -0,0 +1,16 @@
+package com.dang.array;
+
+public class LowArray {
+
+	private long[] a;
+	public LowArray(int size) {
+		a = new long[size];
+	}
+	public void setElem(int index, long value) {
+		a[index] = value;
+	}
+	
+	public long getElem(int index) {
+		return a[index];
+	}
+}
